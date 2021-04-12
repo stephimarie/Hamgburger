941b60be04529c002be209652f6de67e1372643f
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:37:17 -0700

    Sql seeds

---
+++ DB/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUE ('Steak Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUE ('Vegie Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUE ('Salmon Burger', false);

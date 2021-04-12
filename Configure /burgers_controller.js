23b92f5b58d6af85f2cf177c6c8f27c6844e6fea
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:25:11 -0700

    set up controller

--- Configure /burgers_controller.js
+++ Configure /burgers_controller.js
@@ -3,3 +3,13 @@ const router = express.Router();
 const burger = require("../Models/burger.js");
 // fix link
 
+// Create all our routes 
+router.get("/", (req, res) => {
+    burger.all((data) => {
+      const hbsObject = {
+        burger: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });

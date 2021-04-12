50e1867ca8762c2474d675008eec9b55364486e6
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:39:01 -0700

    new file/burger create

--- Models/burger.js
+++ Models/burger.js
@@ -1,3 +1,26 @@
 const orm = require("../Config/orm.js");
 // fix link 
 
+const burger = {
+    all(cb) {
+      orm.all("burger", (res) => {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create(cols, vals, cb) {
+      orm.create("burger", cols, vals, (res) => {
+        cb(res);
+      });
+    },
+    update(objColVals, condition, cb) {
+      
+      orm.update("burger", objColVals, condition, (res) => {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller.
+  module.exports = burger;
+  

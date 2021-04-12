3fe3e9f94513a04978f4070ab5d13d8e8001a99a
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:34:17 -0700

    insert/values/update/set/where

--- Configure /orm.js
+++ Configure /orm.js
@@ -34,3 +34,58 @@ if (Object.hasOwnProperty.call(ob, key)) {
 // translate array of strings to a single comma-separated string
 return arr.toString();
 }
+
+// Object for all our SQL statement functions.
+const orm = {
+    all(tableInput, cb) {
+      let queryString = `SELECT * FROM ${tableInput};`;
+      connection.query(queryString, (err, result) => {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+
+    create(table, cols, vals, cb) {
+        let queryString = `INSERT INTO ${table}`;
+    
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, (err, result) => {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+
+// An example of objColVals 
+    update(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+            throw err;
+            }
+            cb(result);
+            });
+    }
+};
+
+// Export the orm object for the model.
+module.exports = orm;

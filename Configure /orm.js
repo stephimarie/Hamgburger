40879b2c53662f6e0c0c04e32a22457bf4128fd6
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:29:49 -0700

    orm set up

--- Configure /orm.js
+++ Configure /orm.js
@@ -1,3 +1,14 @@
 const connection = require("../Config/connection.js");
 //  fix link
 
+// Helper function for SQL syntax.
+
+function printQuestionMarks(num) {
+    let arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }

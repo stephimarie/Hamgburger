84110575f1fa1ea901df9ff6e6fa7d09514dbcf9
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:32:44 -0700

    string call

--- Configure /orm.js
+++ Configure /orm.js
@@ -13,6 +13,24 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
 
-   // Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
     let arr = [];
+// loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+      let value = ob[key];
+
+// check to skip hidden properties
+if (Object.hasOwnProperty.call(ob, key)) {
+    
+    if (typeof value === "string" && value.indexOf(" ") >= 0) {
+      value = "'" + value + "'";
+    }
+    
+    arr.push(key + "=" + value);
+  }
+}
+
+// translate array of strings to a single comma-separated string
+return arr.toString();
+}

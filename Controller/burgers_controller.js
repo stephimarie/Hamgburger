2b66290b1dffce90d46de478f34b8ceb8478ab96
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:48:34 -0700

    burger.js

--- Controller/burgers_controller.js
+++ Controller/burgers_controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const burger = require("../Models/burger.js");
+const burger = require("../Models/burger");
 // fix link
 
 // Create all our routes 

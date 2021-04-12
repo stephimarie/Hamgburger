1de055a60290948291db85dbb5857c62fb7ba335
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:36:29 -0700

    Database set up

---
+++ DB/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create table --
+
+CREATE TABLE burger (
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR (255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
+

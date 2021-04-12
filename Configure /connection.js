29ec854c528589256088c4dd21c5b995cf766c60
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:28:16 -0700

    connection set up

--- Configure /connection.js
+++ Configure /connection.js
@@ -1,1 +1,21 @@
 const mysql = require('mysql');
+
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+  });
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection.
+module.exports = connection;
+

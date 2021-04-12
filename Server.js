401e12fa4c75a010181df5154f9431a051c72617
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-11 23:21:32 -0700

    set up server

--- Server.js
+++ Server.js
@@ -6,3 +6,24 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 2901;
 
 const app = express();
+
+app.use(express.static('Public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// fix this 
+const routes = require('./Controller/burgers_controller');
+app.use(routes);
+
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
